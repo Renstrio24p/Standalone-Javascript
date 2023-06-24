31b583304b0f729ac0eb05c2d12b768e4adadb6a
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-25 02:34:43 +0800

    add files

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -3,7 +3,10 @@ import styles from '../sass/modules/app.module.scss';
 export default function Footer(Footer){
 
     Footer.innerHTML = `
-        <span>Version 1.0.2</span>
+        <div class=${styles['d-flex-between']}>
+            <span>Version 1.0.2</span>
+            <img class=${styles['sass_img']} src='./src/images/sass.png' alt='sass logo' />
+        </div>
         <div class=${styles['d-flex-between']}>
             <img class=${styles['github_img']} src='./src/images/github.png' alt='github' />
             <span>Codespaces</span>
