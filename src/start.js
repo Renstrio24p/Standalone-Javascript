0d8afa4a08e1a9e5bd538118e796101c80de196c
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-26 02:34:41 +0800

    add code

--- src/start.js
+++ src/start.js
@@ -15,9 +15,22 @@ export default function Start(DOM) {
         <div id='container' class=${cx('d-flex-center-max')}>
 
             <div class=${cx('d-flex-center-gap-20')}>
-                <a href="https://javascript.info/"><img class=${cx('js-image')} src='./src/images/javascript.png' alt='javascript logo'/></a>
+                <a href="https://javascript.info/">
+                <img 
+                    class=${cx('js-image')} 
+                    src='./src/images/javascript.png' 
+                    alt='javascript logo'
+                    width="100"
+                    height="100"    
+                /></a>
                 <h1 class=${cx('font-weight-400')}>Standalone - Javascript ES10</h1>
-                <a href="https://webpack.js.org/"><img class=${cx('webpack-image')} src='./src/images/webpack.png' alt='webpack logo'/></a>
+                <a href="https://webpack.js.org/">
+                    <img class=${cx('webpack-image')} 
+                         src='./src/images/webpack.png' 
+                         alt='webpack logo'
+                         width="150"
+                         height="150"  
+                    /></a>
             </div>
             <div>
                 <button class=${cx('button-style')} id='count'></button>
