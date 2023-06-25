13a405826d19d1cddca7f440bf2d8ef1e49517ad
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-06-25 18:35:23 +0000

    deploy: 0d8afa4a08e1a9e5bd538118e796101c80de196c

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -5,10 +5,10 @@ export default function Footer(Footer){
     Footer.innerHTML = `
         <div class=${cx('d-flex-between')}>
             <span>Version 1.0.3</span>
-            <img class=${cx('sass_img')} src='./src/images/sass.png' alt='sass logo' />
+            <img class=${cx('sass_img')} src='./src/images/sass.png' alt='sass logo' width="70" height="50"/>
         </div>
         <div class=${cx('d-flex-between')}>
-            <img class=${cx('github_img')} src='./src/images/github.png' alt='github' />
+            <img class=${cx('github_img')} src='./src/images/github.png' alt='github' width="100" height="40" />
             <span>Codespaces</span>
         </div>
     `
