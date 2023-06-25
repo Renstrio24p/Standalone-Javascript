0a252c20cc8b8d6c7c03b179ba54d7243781a741
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-26 02:18:32 +0800

    add files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Start from "./start";
 import './sass/index.scss';
 import UniqueHash from "./security/HashCode";
 
-const DOM = document.querySelector('#root');
+const DOM = document.querySelector('#dom');
 DOM.id = UniqueHash();
 Start(DOM); // Renders the Element ID
