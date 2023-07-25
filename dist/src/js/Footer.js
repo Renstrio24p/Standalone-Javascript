85197f78f5915dfffa40820c99ce78a28ff040d6
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-07-25 08:41:39 +0800

    update files

---
+++ dist/src/js/Footer.js
@@ -0,0 +1,15 @@
+import { cx } from '../start';
+
+export default function Footer(Footer){
+
+    Footer.innerHTML = `
+        <div class=${cx('d-flex-between')}>
+            <span>Version 1.0.3</span>
+            <img class=${cx('sass_img')} src='./src/images/sass.png' alt='sass logo' />
+        </div>
+        <div class=${cx('d-flex-between')}>
+            <img class=${cx('github_img')} src='./src/images/github.png' alt='github' />
+            <span>Codespaces</span>
+        </div>
+    `
+}
---
+++ dist/src/start.js
@@ -0,0 +1,43 @@
+import classNames from "classnames/bind";
+import { ES10 } from "./js/es10";
+import styles from './sass/modules/app.module.scss'
+import Render from "./security/Render";
+
+export const cx = classNames.bind(styles);
+
+export default function Start(DOM) {
+    // Function here
+    
+    ES10();
+ 
+    DOM.innerHTML = (
+        `
+        <div id='container' class=${cx('d-flex-center-max')}>
+
+            <div class=${cx('d-flex-center-gap-20')}>
+                <a href="https://javascript.info/">
+                <img 
+                    class=${cx('js-image')} 
+                    src='./src/images/javascript.png' 
+                    alt='javascript logo'   
+                /></a>
+                <h1 class=${cx('font-weight-400')}>Standalone - Javascript</h1>
+                <a href="https://webpack.js.org/">
+                    <img class=${cx('webpack-image')} 
+                         src='./src/images/webpack.png' 
+                         alt='webpack logo'
+                    /></a>
+            </div>
+            <div>
+                <button class=${cx('button-style')} id='count'></button>
+            </div>
+            <div  class=${cx('d-flex-gap-20')} id="rows"></div>
+            <footer class=${cx('footer-abs-between')} id="footer"></footer>
+        
+        </div>
+
+        `
+    )
+
+  Render(); // Renders all the Element ID's
+}
