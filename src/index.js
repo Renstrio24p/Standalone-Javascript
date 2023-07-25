79389457570de7a1f8b76717713876883e486cbd
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-07-25 02:00:42 +0000

    deploy: 948154eea13aff48e3b2ffc4c3fe8cada87d49d8

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Start from "./start";
-import './sass/index.scss';
-import UniqueHash from "./security/HashCode";
-
-
-const DOM = document.querySelector('#app');
-DOM.id = UniqueHash();
-Start(DOM); // Renders the Element ID
