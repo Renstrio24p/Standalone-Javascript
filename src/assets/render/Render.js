bf27acca4b5fbf28af00dcc00e253ed7d61cdaa0
Waren Gador <waren.gador15@gmail.com>
2023-10-22 21:49:59 +0800

    update render.js

--- src/assets/render/Render.js
+++ src/assets/render/Render.js
@@ -1,27 +1,38 @@
+async function importAllComponents() {
+  const context = require.context('../../components', true, /\.js$/); // Fetch all .js files
+  const componentModules = await Promise.all(
+    context.keys().map(context)
+  );
+
+  const components = {};
+
+  componentModules.forEach((module) => {
+    const componentName = module.default ? module.default.name : null;
+    if (componentName) {
+      components[componentName] = module.default;
+    }
+  });
+
+  return components;
+}
+
 export default async function Render() {
-    // Dynamically import the components and modules
-    const { default: Footer } = await import("../../components/Footer");
-    const { setupCounter } = await import("../../components/counter");
-    const { default: Rows } = await import("../../components/row");
-    const { default: UniqueHash } = await import("../security/hascode");
-  
-    // Get Render's ID
-    const ContainerDOM = document.getElementById('container');
-    const APP = document.getElementById('count');
-    const Row = document.getElementById('rows');
-    const Foot = document.getElementById('footer');
-  
-    // Hash Applied
-    window.addEventListener('DOMContentLoaded', () => {
-      ContainerDOM.id = UniqueHash();
-      APP.id = UniqueHash();
-      Row.id = UniqueHash();
-      Foot.id = UniqueHash();
-    });
-  
-    // Render's the JS Component
-    setupCounter(APP);
-    Rows(Row);
-    Footer(Foot);
+  const components = await importAllComponents();
+
+  const componentElements = document.querySelectorAll('[component]');
+
+  for (const element of componentElements) {
+    const componentName = element.getAttribute('component');
+    
+    if (components[componentName]) {
+      components[componentName](element);
+    } else {
+      console.warn(`Component ${componentName} not found.`);
+    }
   }
-  
+
+  // Hash Applied (if needed)
+  window.addEventListener('DOMContentLoaded', () => {
+    // Your hash code here
+  });
+}
